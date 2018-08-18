56aac80d05c96165e4831db66ca1231f303ea779
JohnnyDHo <johnnyyyyho1999@gmail.com>
2018-08-17 19:47:12 -0500

    Full scraping template for US News Car Reviews, default on us-news.js is the Toyota Camry, but works with any car, see "us-news.json" in the data folder to see the results.

--- us-news.js
+++ us-news.js
@@ -8,15 +8,38 @@ axios.get('https://cars.usnews.com/cars-trucks/toyota/camry')
             var html = response.data;
             let $ = cheerio.load(html);
             var verdict = [];
+            var other = [];
+            var reliability = [];
+            var ranking = [];
+            var awards = [];
             $('.scorecard__overall-score-container').each(function(i, elem) {
-
                 verdict[i] = {
-                    verdicts: $(this).children('.scorecard__score').text(),
-                    texts: $(this).children('.scorecard__score-label').text()
+                    overall: $(this).children('.scorecard__score-label').text() + ': ' + $(this).children('.scorecard__score').text()
                 }
             });
+            $('.clearfix').each(function(i, elem) {
+                other[i] = {
+                    others: $(this).children('.float-left').text().trim() + ' ' + $(this).children('.float-right').text().trim()
+                }
+            });
+            $('.reliability-row').each(function(i, elem) {
+                reliability[i] = {
+                    reliability: $(this).children('.inline-block').text() + ' ' + $(this).children('.display-block').children('a').children('.float-left').attr('alt')
+                }
+            });
+            $('.rankings__item').each(function(i, elem) {
+                ranking[i] = {
+                    ranking: $(this).children('span').text() + " " + $(this).children('a').text()
+                }
+            });
+            $('.awards__item').each(function(i, elem) {
+                awards[i] = {
+                    award: $(this).text()
+                }
+            });
+            var combined = verdict.concat(other, reliability, ranking, awards);
             fs.writeFile('data/us-news.json',
-                JSON.stringify(verdict, null, 4), (err)=>{
+                JSON.stringify(combined, null, 4), (err)=>{
                     console.log('File successfully written!');
                 })
         }
